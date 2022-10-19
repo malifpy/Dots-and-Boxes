7ab5f94f3fa937be1ff588eef4e04570f9f2514d
ilhampratama2109 <pilham902@gmail.com>
2022-10-19 18:39:54 +0700

    menghapus file yang tidak dibutuhkan

--- Node.py
+++
@@ -1,8 +0,0 @@
-class Node:
-    def __init__(self) -> None:
-        pass
-    
-    def eval():
-        pass #untuk heusritik tiap node, dilihat nilai maks - nilai minnya
-    
-    #belum dibikin implementasinya
